fc3033b62906ba0e6ad753642c825943bad9ba84
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-02 15:52:10 -0300

    Fix

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # express-from-deploy-static-pages
  
+### Fazer o build do Projeto e deixar a pasta /dist na raiz deste projeto
+- Se o nome da pasta for Build, Precisa alterar no codigo
+
+## Usar o PM2 para deploy 
+
+- Exemplo
+
+<img src="image.png" width="100%" height="auto" />
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
-import express from 'express';
-import path from 'path'
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
